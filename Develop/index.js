2bcf36ce51b0bac2f7b276b36709d36bb77576da
LiyuanSilviaZhao <zhao0301liyuan@gmail.com>
2022-10-31 21:15:24 -0700

    Change port number

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const uuid = require('./helpers/uuid');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"))
 
